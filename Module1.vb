f63f3f6b3ba58e1b937ca9a58ceb94d207c1f69c
IGNEEL16 <96214184+IGNEEL16@users.noreply.github.com>
2022-02-08 11:19:45 +0800

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Data.SqlClient
+
+Module Module1
+    Public cn As New SqlConnection
+    Public cmd As New SqlCommand
+    Public da As New SqlDataAdapter
+    Public ds As New DataSet
+    Public str As String
+
+    Public Sub conn()
+        cn = New SqlConnection("Data Source=DESKTOP-2LLV29I\MSSQLSERVER01;Initial Catalog=SIM;Integrated Security=True;Pooling=False")
+
+
+    End Sub
+End Module
